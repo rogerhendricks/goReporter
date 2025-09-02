5044fbc3a2b246d19a3636bc08e1bf8ed28deb8f
Roger Hendricks <hendricks.r@gmail.com>
2025-09-02 18:31:39 +1000

    added different archetecture build commannds to build.bash

--- build.bash
+++ build.bash
@@ -6,6 +6,8 @@ npm run build
 
 echo "Building Go backend..."
 cd ..
-go build -o bin/server cmd/api/main.go
-
+# go build -o bin/server cmd/api/main.go
+GOOS=windows GOARCH=amd64 go build -o bin/server.exe cmd/api/main.go
+# GOOS=linux GOARCH=amd64 go build -o bin/server cmd/api/main.go
+# GOOS=darwin GOARCH=amd64 go build -o bin/server cmd/api/main.go
 echo "Build complete!"
